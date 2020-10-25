3fa242e3d34abbdb4fe56ccc1e82dac895fe62df
Mike Cluck <mike-cluck@pluralsight.com>
2020-10-25 15:49:34 -0600

    Add support for function calls

--- src/ast/call.ts
+++ src/ast/call.ts
@@ -1,10 +1,35 @@
+import { emit } from '../emit';
 import { AST } from '../types';
 
 export class Call implements AST {
   constructor(public callee: string, public args: AST[]) {}
 
   emit() {
-    throw new Error('Not yet implemented');
+    switch (this.args.length) {
+      case 0:
+        emit(`  bl ${this.callee}`);
+        break;
+
+      case 1:
+        this.args[0].emit();
+        emit(`  bl ${this.callee}`);
+        break;
+
+      case 2:
+      case 3:
+      case 4:
+        emit(`  sub sp, sp, #16`);
+        this.args.forEach((arg, i) => {
+          arg.emit();
+          emit(`  str r0, [sp, #${4 * i}]`);
+        });
+        emit(`  pop {r0, r1, r2, r3}`);
+        emit(`  bl ${this.callee}`);
+        break;
+
+      default:
+        throw new Error('More than 4 arguments is not supported');
+    }
   }
 
   equals(other: AST): boolean {
--- tests/integration-tests.ts
+++ tests/integration-tests.ts
@@ -7,6 +7,13 @@ function main() {
   assert(1);
   assert(!0);
   assert(42 == 4 + 2 * (12 - 2) + 3 * (5 + 1));
+
+  {
+    assert(1);
+    assert(1);
+  }
+  putchar(46);
+  assert(rand() != 42);
 }`
   )
   .emit();
